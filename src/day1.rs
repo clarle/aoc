6fda62166ea02216f2ee0ed8ee5c01e13b77f62f
Clarence Leung <cleung@netflix.com>
2021-12-01 01:47:44 -0800

    fix(day1): Lint warnings

--- src/day1.rs
+++ src/day1.rs
@@ -10,7 +10,7 @@ fn parse_input_day1(input:&str) -> Result<Vec<i32>, ParseIntError> {
 pub fn part1(depths: &[i32]) -> i32 {
     let windows = depths.windows(2).collect::<Vec<_>>();
     return windows.iter().fold(0, |sum, window| {
-        if (window[1] > window[0]) {
+        if window[1] > window[0] {
             return sum + 1;
         } else {
             return sum;
@@ -23,9 +23,9 @@ pub fn part2(depths: &[i32]) -> i32 {
     let windows = depths.windows(3).collect::<Vec<_>>();
     let window_pairs = windows.windows(2).collect::<Vec<_>>();
     return window_pairs.iter().fold(0, |sum, pair| {
-        let sumOne: i32 = pair[0].iter().sum();
-        let sumTwo: i32 = pair[1].iter().sum();
-        if (sumTwo > sumOne) {
+        let sum_one: i32 = pair[0].iter().sum();
+        let sum_two: i32 = pair[1].iter().sum();
+        if sum_two > sum_one {
             return sum + 1;
         } else {
             return sum;
